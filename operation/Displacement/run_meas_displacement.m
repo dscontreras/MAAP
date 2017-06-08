674efe0dda0aec401ef967e3039c224533307c30
Timothy Liu <timmytimmyliu@Timothys-MacBook-Pro.local>
2017-06-08 13:30:10 -0700

    pull

--- operation/Displacement/run_meas_displacement.m
+++ operation/Displacement/run_meas_displacement.m
@@ -4,7 +4,7 @@ vHeight = v.Height;
 rect = [730, 550, 70, 30];
 originalFrame = rgb2gray(readFrame(v));
 k = 1;
-while v.hasFrame & k < 120
+while v.hasFrame 
     frame = readFrame(v);
     frame = rgb2gray(frame); 
     mov(k).cdata = frame;
@@ -15,14 +15,15 @@ img = mov(21).cdata;
 displacement = 50;
 xtemp = rect(1);
 ytemp = rect(2);
-precision = 1;
-res = 1;
-%tic
+precision = rand;
+res = rand;
+tic
 [xoffSet, yoffSet, dispx,dispy,x, y, c1] = meas_displacement(template, rect, img, xtemp, ytemp, precision, displacement, res);
-%toc
-%tic
+toc
+tic
 [xoffSet1, yoffSet1, dispx1,dispy1,x1, y1, c11] = meas_displacement2(template, rect, img, xtemp, ytemp, precision, displacement, res);
-%toc
+toc
+
 
 xoffSet == xoffSet1
 yoffSet == yoffSet1
@@ -30,5 +31,6 @@ dispx == dispx1
 dispy == dispy1
 x1 == x
 y == y1
-c1 == c11
+c1 == c11 
+
 
