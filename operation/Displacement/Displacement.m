c96141e644dc83635efe6c34170ebd853ebb89d4
Hyunkyu Lee <hyunkyu@berkeley.edu>
2017-06-18 11:53:07 -0700

    Fix runtime errors, fourier output currently incorrect

--- operation/Displacement/Displacement.m
+++ operation/Displacement/Displacement.m
@@ -22,15 +22,18 @@ classdef Displacement < RepeatableOperation
         xoff;
         yoff;
         
-        % 
+        % Properties needed for fourier analysis
         search_area_width; search_area_height;
-        search_area_xmin; search_area_ymin;
+        search_area_xmin;  search_area_ymin;
         min_displacement; 
         
-        % Properties for use when using fft2
-        fft_conj_processed_template;
-        processed_template; 
-        template_mean; % Average value of template
+        template_average;
+        processed_template; fft_conj_processed_template; 
+        
+        interp_search_area_width; interp_search_area_height; 
+        interp_template; interp_template_average;
+        processed_interp_template; fft_conj_processed_interp_template;
+        
     end
 
     properties (SetAccess = public)
@@ -95,25 +98,41 @@ classdef Displacement < RepeatableOperation
             obj.current_frame = gather(grab_frame(obj.vid_src, obj));
             [obj.template, obj.rect, obj.xtemp, obj.ytemp] = get_template(obj.current_frame, obj.axes);
             obj.rect = ceil(obj.rect); 
-            
-            % 
-            obj.search_area_height  = 2 * obj.max_displacement + obj.rect(3);
-            obj.search_area_width   = 2 * obj.max_displacement + obj.rect(4);
+             
+            obj.search_area_height  = 2 * obj.max_displacement + obj.rect(3) + 1; % imcrop will add 1 to the dimension
+            obj.search_area_width   = 2 * obj.max_displacement + obj.rect(4) + 1; % imcrop will add 1 to the dimension
             obj.search_area_xmin    = obj.rect(1) - obj.max_displacement;
-            obj.search_area_ymin    = obj.rect(2) - obj.max_displacement;
+            obj.search_area_ymin    = obj.rect(2) - obj.max_displacement;  
             
-            % Begin Fourier Method Code
-            obj.search_area_width   = 2*obj.max_displacement + obj.rect(3);
-            obj.search_area_height  = 2*obj.max_displacement + obj.rect(4);
-            obj.template_mean       = mean(mean(obj.template));
+            
+            obj.template_average    = mean(mean(obj.template));
+            obj.processed_template  = obj.template_average - obj.template; 
+            obj.fft_conj_processed_template = conj(fft2(obj.processed_template, obj.search_area_height*2, obj.search_area_width*2));
+            
+            % The frame we will crop out when interpolating will have size (template_height + 2*min + 1, template_width+2*min+1)
+            % Interpolation takes that and the resulting dimension size is (k - 1)/obj.pixel_precision + 1; 
+            obj.interp_search_area_width 	= (obj.rect(4) + 2 * obj.min_displacement + 1 - 1)/obj.pixel_precision + 1;
+            obj.interp_search_area_height 	= (obj.rect(3) + 2 * obj.min_displacement + 1 - 1)/obj.pixel_precision + 1;
+            
+            obj.interp_template = im2double(obj.template);
+            numRows 			= obj.rect(4);
+            numCols 			= obj.rect(3);
+            [X, Y] 				= meshgrid(1:numCols, 1:numRows);
+            [Xq, Yq] 			= meshgrid(1:obj.pixel_precision:numCols, 1:obj.pixel_precision:numRows);
+            V 					= obj.interp_template;
+            obj.interp_template = interp2(X, Y, V, Xq, Yq, 'cubic');
+
+            obj.interp_template_average = mean(mean(obj.interp_template));
+            obj.processed_interp_template = obj.interp_template_average - obj.interp_template;
+            obj.fft_conj_processed_interp_template = conj(fft2(obj.processed_interp_template, obj.interp_search_area_height*2, obj.interp_search_area_width*2));
             
             % Make sure that when darks match up in both the
             % image/template, they count toward the correlation
             % We subtract from the mean as we know that darks are the
             % edges. 
-            obj.processed_template  = obj.template_mean - obj.template; 
+            obj.processed_template  = obj.template_average - obj.template; 
             
-            obj.fft_conj_processed_template = conj(fft2(obj.processed_template, obj.search_area_height, obj.search_area_width));
+            obj.fft_conj_processed_interp_template = conj(fft2(obj.processed_template, obj.interp_search_area_height*2, obj.interp_search_area_width*2));
         end
         
         function execute(obj)  
@@ -123,8 +142,8 @@ classdef Displacement < RepeatableOperation
                     % [xoffSet, yoffSet, dispx,dispy,x, y] = meas_displacement_gpu_array(obj.template,obj.rect,obj.current_frame, obj.xtemp, obj.ytemp, obj.max_displacement, obj.res);
                     % [xoffSet, yoffSet, dispx,dispy,x, y] = meas_displacement_subpixel_gpu_array(obj.template,obj.rect,obj.current_frame, obj.xtemp, obj.ytemp, obj.pixel_precision, obj.max_displacement, obj.res);
                 else
-                    [xoffSet, yoffSet, dispx,dispy,x, y] = obj.meas_displacement();
-                    % [xoffSet, yoffSet, dispx,dispy,x, y] = meas_displacement_fourier(obj.template,obj.rect,obj.current_frame, obj.xtemp, obj.ytemp, obj.pixel_precision, obj.max_displacement, obj.res, obj.fft_conj_template);
+                    % [xoffSet, yoffSet, dispx,dispy,x, y] = obj.meas_displacement();
+                    [xoffSet, yoffSet, dispx,dispy,x, y] = obj.meas_displacement_fourier();
                 end
             else
                 if(obj.vid_src.gpu_supported)
@@ -220,34 +239,22 @@ classdef Displacement < RepeatableOperation
         
         function [xoffSet, yoffSet, dispx,dispy,x, y] = meas_displacement_fourier(obj)
             %% Whole Pixel Precision Coordinates
+            img = obj.current_frame;
+            [search_area, ~] = imcrop(img,[obj.search_area_xmin, obj.search_area_ymin, obj.search_area_width, obj.search_area_height]);
             
-            [ypeak, xpeak] = obj.fourier_cross_correlation(obj.fft_conj_processed_template, search_area, search_area_height, search_area_width, rect, 120);
+            [ypeak, xpeak] = obj.fourier_cross_correlation(obj.fft_conj_processed_template, search_area, obj.search_area_height, obj.search_area_width);
             
-            % Put the new min values relative to img, not search_area
-            new_xmin = xpeak+round(search_area_rect(1))-1; 
-            new_ymin = ypeak+round(search_area_rect(2))-1; 
+            new_xmin = xpeak;
+            new_ymin = ypeak;
         
             %% Subpixel Precision Coordinates TODO: Refractor? This looks like a repetitive calculation
-            [moved_templated, displaced_rect] = imcrop(img, [new_xmin new_ymin, obj.rect(3) obj.rect(4)]);
             
-            new_search_area_xmin = new_xmin - obj.min_displacement;
-            new_search_area_ymin = new_ymin - obj.min_displacement; 
-            new_search_area_width = 2*obj.min_displacement  + obj.rect(3);
-            new_search_area_height = 2*obj.min_displacement + obj.rect(4);
-            
-            [new_search_area, new_search_area_rect] = imcrop(img, [new_search_area_xmin new_search_area_ymin new_search_area_width new_search_area_height]);
+            [new_search_area, new_search_area_rect] = imcrop(search_area, [new_xmin new_ymin obj.interp_search_area_width obj.interp_search_area_height]);
             
             %% Interpolation
             %Interpolate both the new object area and the old and then compare
             %those that have subpixel precision in a normalized cross
             %correlation
-            % BICUBIC INTERPOLATION - TEMPLATE
-            interp_template = im2double(obj.template);
-            [numRows,numCols,~] = size(interp_template); % Replace with rect? 
-            [X,Y] = meshgrid(1:numCols,1:numRows); %Generate a pair of coordinate axes
-            [Xq,Yq]= meshgrid(1:obj.pixel_precision:numCols,1:obj.pixel_precision:numRows); %generate a pair of coordinate axes, but this time, increment the matrix by 0
-            V=interp_template; %copy interp_template into V
-            interp_template = interp2(X,Y,V,Xq,Yq, 'cubic');
 
             % BICUBIC INTERPOLATION - SEARCH AREA (FROM MOVED TEMPLATE
             interp_search_area = im2double(new_search_area);
@@ -257,10 +264,12 @@ classdef Displacement < RepeatableOperation
             V=interp_search_area;
             interp_search_area = interp2(X,Y,V,Xq,Yq, 'cubic');
             
-            c1 = normxcorr2(interp_template, interp_search_area);
-            [new_ypeak, new_xpeak] = find(c1==max(c1(:)));
+            processed_interp_search_area = obj.interp_template_average - interp_search_area;
+            
+            [new_ypeak, new_xpeak] = obj.fourier_cross_correlation(obj.fft_conj_processed_interp_template, processed_interp_search_area, obj.interp_search_area_height, obj.interp_search_area_width);
             new_xpeak = new_xpeak/(1/obj.pixel_precision);
             new_ypeak = new_ypeak/(1/obj.pixel_precision);
+            
             new_xpeak = new_xpeak+round(new_search_area_rect(1));
             new_ypeak = new_ypeak+round(new_search_area_rect(2));
             
@@ -279,7 +288,7 @@ classdef Displacement < RepeatableOperation
         
         % Finds the cross correlation using fourier transforms
         % TODO: implement ability to be used for interpolated images
-        function [xpeak, ypeak] = fouier_cross_correlation(obj, fft_conj_template, search_area)
+        function [ypeak, xpeak] = fourier_cross_correlation(obj, fft_conj_template, search_area, height, width)
                 
                 % TEMPLATE and SEARCH_AERA should be grayscaled images
                 % Performs cross correlation of SEARCH_AREA and TEMPLATE
@@ -291,10 +300,13 @@ classdef Displacement < RepeatableOperation
                 % These values were gotten experimentally, they may need to be tweaked
 
                 % Some image preprocessing; Assumes an 8bit grayscale image
-                search_area = obj.template_mean - search_area;
+                % TODO :change obj.template average? 
+                % In general, obj.template_average and obj.interp_average aren't
+                % too different so perhaps not needed
+                search_area = obj.template_average - search_area;
                 
                 % Find the DTFT
-                dtft_of_frame = fft2(search_area);
+                dtft_of_frame = fft2(search_area, height*2, width*2);
                 
                 % Take advantage of the correlation theorem
                 % Corr(f, g) <=> element multiplication of F and conj(G) where F, G are
