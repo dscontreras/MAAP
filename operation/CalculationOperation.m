63d00603a82a367787f0ab574d82b147d0ffcb90
Hyunkyu Lee <hyunkyu@berkeley.edu>
2017-06-21 10:46:57 -0700

    Adds a subclass of Operation: CalculationOperation

---
+++ operation/CalculationOperation.m
@@ -0,0 +1,12 @@
+classdef (Abstract) CalculationOperation < Operation
+    %CalculationOperation 
+    %An abstract type to represent operations that do a series of
+    %calculatons. Does not collect or save the data in any way. 
+    
+    properties(Access = public, Abstract)
+        % Source from which the data is to be extracted. This can be
+        % anything, a video file, livestream, etc.
+        % TODO: In the future, force this to be of a certain SourceType. 
+        source; 
+    end  
+end
