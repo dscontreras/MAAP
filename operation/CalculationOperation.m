7da876544bff03c3a61a038daef3c6be8dd8e8b4
Hyunkyu Lee <hyunkyu@berkeley.edu>
2017-06-22 12:45:36 -0700

    Fix DisplacementOperation constructor

--- operation/CalculationOperation.m
+++ operation/CalculationOperation.m
@@ -3,16 +3,16 @@ classdef (Abstract) CalculationOperation < Operation
     %An abstract type to represent operations that do a series of
     %calculatons. Does not collect or save the data in any way.
 
-    properties(Access = protected, Abstract)
+    properties(Abstract)
         % Source from which the data is to be extracted. This can be
         % anything, a video file, livestream, etc.
         % TODO: In the future, force this to be of a certain SourceType.
         source;
-        % A list of operation to complete. This may be useful for
-        % operations that would like to use an existing operation.
     end
 
-    properties(Access = private)
+    properties(Access = protected)
+        % A list of operation to complete. This may be useful for
+        % operations that would like to use an existing operation.
         operation = Operation.empty()
     end
 end
