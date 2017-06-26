8ba5455ca9d44b30612175b33ac07bbe496fe87a
Timothy Liu <tliu12@berkeley.edu>
2017-06-26 09:11:14 -0700

    working on velocity

--- ui/Velocity.m
+++ ui/Velocity.m
@@ -5,12 +5,22 @@ classdef Velocity < RepeatableOperation
     
     properties (SetAccess = private)
         scale_img; % image that contains scale
-        displacement;
         calibration; % converts px to um
         scale_length;
         img_cover;
         pause_button;
         stop_check_callback = @check_stop;
+        vid_src;
+        axes;
+        table;
+        error; 
+        pixel_precision;
+        max_displacement; 
+        res;
+        xoff; yoff;
+        table_data;
+        current_frame;
+        im;
     end
     
     properties (SetAccess = public)
@@ -27,8 +37,8 @@ classdef Velocity < RepeatableOperation
 
     properties (Constant)
         rx_data = {};
-        name = 'Displacement';
-        insertion_type = 'start'; %%% or end?
+        name = 'Velocity';
+        insertion_type = 'end'; 
         camera_fps = 15; % Pister lab camera fps
     end
     
@@ -36,43 +46,57 @@ classdef Velocity < RepeatableOperation
         function obj = Velocity(src, axes, table, error, img_cover, pause_button,... 
                 pixel_precision, max_displacement, resolution, ...
                 scale_img, scale_length, error_report_handle)
-            
-            obj.scale_img = scale_img;
-            obj.displacement = Displacement(src, axes, table, error, ...
-                img_cover, pause_button, pixel_precision, max_displacement,...
-                resolution, draw, error_report_handle);
-            
+            obj.scale_img = imread(scale_img);
             obj.scale_length = scale_length;
             obj.img_cover = img_cover;
             obj.pause_button = pause_button;
+            obj.vid_src = src;
+            obj.new = true;
+            obj.valid = true;
+            obj.axes = axes;
+            obj.table = table;
+            obj.error = error;
+            obj.pixel_precision = str2double(pixel_precision);
+            obj.max_displacement = str2double(max_displacement);
+            obj.res = resolution;
+            obj.outputs('dispx') = 0;
+            obj.outputs('dispy') = 0;
+            obj.outputs('velocity') = 0;
+            obj.outputs('done') = false;
+            obj.xoff = [];
+            obj.yoff = [];
+            if(nargin > 11) % 11 is the number of params for velocity
+                obj.error_report_handle = error_report_handle;
+            end   
         end
         
         %For carrying out one time method calls that should be done before
         %calling of execute
         function startup(obj)
             set(obj.img_cover, 'Visible', 'Off');
+            %imshow(obj.scale_img);
             set(obj.pause_button, 'Visible', 'On');
-            obj.initialize_algorithm();
-            %obj.displacement.startup();
+            obj.table_data = {'DispX'; 'DispY'; 'Velocity'};
+            obj.im = zeros(obj.vid_src.get_num_pixels());
+            distance = get_line(obj.scale_img, obj.axes);
+            obj.calibration = obj.scale_length / distance;
+            %obj.im = imshow(obj.im);
         end  
         
-        function initialize_algorithm(obj)
-            imshow(obj.scale_img);
-            scale = imdistline;
-            user_input = iptgetapi(scale);
-            dist_in_pixels = user_input.getDistance();
-            obj.calibration = obj.scale_length / dist_in_pixels;
-            obj.finished();
+        function execute(obj)
+            return;
         end
         
-        
-        
         function bool = check_stop(obj)
-            bool = (~obj.valid && ~obj.validate(obj.error_tag)) | obj.vid_src.finished();
+            bool = true;
         end
         
         function valid = validate(obj, error_tag)
             valid = obj.displacement.validate(error_tag);
+        end
+        
+        function bool = paused(obj)
+            bool = false;
         end
     end    
 end
--- ui/data_gui.m
+++ ui/data_gui.m
@@ -912,7 +912,6 @@ function begin_operation_btn_Callback(begin_measurement_btn, eventdata, handles)
             pixel_precision, max_displacement, res, draw);        
         %displacement = Displacement(src, handles.img_viewer, handles.data_table, handles.vid_error_tag, handles.image_cover, handles.pause_operation, pixel_precision, max_displacement, res, draw);
     elseif get(handles.velocity_check, 'Value') == 1
-        "here"
         res_entry_obj = findobj('Tag', 'source_resolution_entry');
         resolution = res_entry_obj.UserData;
         if(isnumeric(resolution) && ~isempty(resolution) && resolution > 0)
