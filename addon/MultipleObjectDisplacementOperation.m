48b2b5e425903cfc238ffe0686929b3b0b5cc8f9
Hyunkyu Lee <hyunkyu@berkeley.edu>
2017-07-05 14:13:19 -0700

    Adds class that will tractk more than one object at a time

---
+++ addon/MultipleObjectDisplacementOperation.m
@@ -0,0 +1,10 @@
+classdef MultipleObjectDisplacementOperation < DisplacementOperation
+
+    methods
+        function obj = MultipleObjectDisplacementOperation(src, axes, table, error, img_cover, pause_button, pixel_precision,
+            max_displacement, resolution, draw, error_report_handle)
+
+        end
+    end
+
+end
