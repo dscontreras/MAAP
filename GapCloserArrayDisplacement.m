3ea15ace003fd294ab597eff3312b573c9980bee
Hyunkyu Lee <hyunkyu@berkeley.edu>
2017-07-14 10:25:59 -0700

    Adds a displacement measurer for GCAs

---
+++ GapCloserArrayDisplacement.m
@@ -0,0 +1,16 @@
+classdef GapCloserArrayDisplacement < Operation
+
+properties
+    source
+end
+
+methods
+    function obj = GapCloserArrayDisplacement(src, template_file_path)
+        obj.source = src;
+    end
+
+
+
+end
+
+end
