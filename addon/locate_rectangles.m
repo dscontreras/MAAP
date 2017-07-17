300e6792adb7c87268cbef5513fdb611bc867aa5
Hyunkyu Lee <hyunkyu@berkeley.edu>
2017-07-17 11:44:11 -0700

    Change name of locate_squares

--- addon/locate_rectangles.m
+++ addon/locate_rectangles.m
@@ -1,9 +1,9 @@
 % Given an image, attempts to find the squares that are present in many of the objects that are being studied such as the GCA
 % x, y, width, height represents the `rect` that would be used for template matching
 
-% SQUARES will have some amount of squares. Access each square as you would any array. Access the elements of the squares by getting the BoundingBox. 
+% Rectangles will have some amount of rectangles. Access each square as you would any array. Access the elements of the rectangles by getting the BoundingBox. 
 % The BoundingBox is of the form [xmin ymin width height]
-function [squares] = location_squares(img)
+function [rectangles] = location_rectangles(img)
     % Convert to grayscale if necessary
     if length(size(img)) == 3
         img = rgb2gray(img);
@@ -15,6 +15,6 @@ function [squares] = location_squares(img)
     Iarea = bwareaopen(Ifill,100);
     Ifinal = bwlabel(Iarea);
     stat = regionprops(Ifinal,'boundingbox');
-    squares = stat;
+    rectangles = stat;
 end
     
