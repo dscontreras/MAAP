bdf4b3c60e8c8f172ab0a0e94b7cbd83399e7bdd
Hyunkyu Lee <hyunkyu@berkeley.edu>
2017-08-03 11:53:54 -0700

    Add comments

--- util/add_headers.m
+++ util/add_headers.m
@@ -1,4 +1,7 @@
 function add_headers(csv_file, varargin)
+    % Adds headers to the csv file
+    % Either pass in the strings like so: add_header(file, 'h1', 'h2', ...)
+    % or as one long string with the format 'h1,h2,h3'
     header_string = varargin{1}; % Assumes at least 1 input
     nVarargs = length(varargin);
     for k = 2:nVarargs
